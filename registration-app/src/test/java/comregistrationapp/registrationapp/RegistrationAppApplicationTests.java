dd60920ece395ca0e0a3e251a2dc03cf58e02447
dorotajachtoma <jachtoma.do@gmail.com>
2021-12-13 19:48:05 +0000

    add project based on spring boot 2.6.1

---
+++ registration-app/src/test/java/comregistrationapp/registrationapp/RegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package comregistrationapp.registrationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
